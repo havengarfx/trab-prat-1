3d45b134e90c661cae83e3417545bce6c296e230
havengarfx <havengarfx@gmail.com>
2020-07-15 22:23:19 +0000

    Capture of reference finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
 console.log("trab-prat-1");
+
+var rangeRed = document.querySelector("#rangeRed");
+var rangeReadRed = document.querySelector("#rangeReadRed");
+rangeReadRed.value = rangeRed.value;
+
+var rangeGreen = document.querySelector("#rangeGreen");
+var rangeReadGreen = document.querySelector("#rangeReadGreen");
+rangeReadGreen.value = rangeGreen.value;
+
+var rangeBlue = document.querySelector("#rangeBlue");
+var rangeReadBlue = document.querySelector("#rangeReadBlue");
+rangeReadBlue.value = rangeBlue.value;
+
+
+var rgb = "rgb"+"("+rangeReadRed.value+","+rangeReadGreen.value+","+rangeReadBlue.value+")";
+console.log(rgb);
+
+var squareBackcolor = document.querySelector(".classe4");
+squareBackcolor.style.background = rgb;
