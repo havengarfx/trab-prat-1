07a3d8f9635b7ae2efc66faaa4fd75d19274aeb6
havengarfx <havengarfx@gmail.com>
2020-07-16 08:27:29 +0000

    Project done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,51 @@
-console.log("trab-prat-1");
+window.addEventListener("load", start);
 
-var rangeRed = document.querySelector("#rangeRed");
-var rangeReadRed = document.querySelector("#rangeReadRed");
-rangeReadRed.value = rangeRed.value;
+function start() {
+  console.log("trab-prat-1");
 
-var rangeGreen = document.querySelector("#rangeGreen");
-var rangeReadGreen = document.querySelector("#rangeReadGreen");
-rangeReadGreen.value = rangeGreen.value;
+  var rangeRed = document.querySelector("#rangeRed");
+  var rangeReadRed = document.querySelector("#rangeReadRed");
+  rangeReadRed.value = rangeRed.value;
 
-var rangeBlue = document.querySelector("#rangeBlue");
-var rangeReadBlue = document.querySelector("#rangeReadBlue");
-rangeReadBlue.value = rangeBlue.value;
+  var rangeGreen = document.querySelector("#rangeGreen");
+  var rangeReadGreen = document.querySelector("#rangeReadGreen");
+  rangeReadGreen.value = rangeGreen.value;
 
+  var rangeBlue = document.querySelector("#rangeBlue");
+  var rangeReadBlue = document.querySelector("#rangeReadBlue");
+  rangeReadBlue.value = rangeBlue.value;
 
-var rgb = "rgb"+"("+rangeReadRed.value+","+rangeReadGreen.value+","+rangeReadBlue.value+")";
-console.log(rgb);
+  var rgb =
+    "rgb" +
+    "(" +
+    rangeReadRed.value +
+    "," +
+    rangeReadGreen.value +
+    "," +
+    rangeReadBlue.value +
+    ")";
+  console.log(rgb);
 
-var squareBackcolor = document.querySelector(".classe4");
-squareBackcolor.style.background = rgb;
+  var squareBackcolor = document.querySelector("#divsquare");
+  squareBackcolor.style.background = rgb;
+
+  rangeRed.addEventListener("input", changeRange);
+  rangeGreen.addEventListener("input", changeRange);
+  rangeBlue.addEventListener("input", changeRange);
+
+  function changeRange() {
+    rangeReadRed.value = rangeRed.value;
+    rangeReadGreen.value = rangeGreen.value;
+    rangeReadBlue.value = rangeBlue.value;
+    var rgb =
+      "rgb" +
+      "(" +
+      rangeReadRed.value +
+      "," +
+      rangeReadGreen.value +
+      "," +
+      rangeReadBlue.value +
+      ")";
+    squareBackcolor.style.background = rgb;
+  }
+}
